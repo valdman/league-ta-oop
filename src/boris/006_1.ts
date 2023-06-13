200aaf606762122c21d05d37f66b95bc6ccf903d
Julie Cherner <juliechernen@gmail.com>
2023-06-13 13:19:51 +0300

    Hometask 5 is completed

--- src/boris/006_1.ts
+++ src/boris/006_1.ts
@@ -10,17 +10,64 @@ let i = 0;
 
 function exampleAsyncFunction(isBroken: boolean) {
   return new Promise((resolve, reject) => {
-	setTimeout(() => {
-	  resolve('Hello world! ' + i++);
-	}, 1000);
+    setTimeout(() => {
+      resolve("Hello world! " + i++);
+    }, 1000);
   });
 }
 
 async function exampleUsage() {
-	const a = await exampleAsyncFunction(false);
-	const b = await exampleAsyncFunction(true);
+  const a = await exampleAsyncFunction(false);
+  const b = await exampleAsyncFunction(true);
 
-	console.log(a, b);
+  console.log(a, b);
 }
 
 exampleUsage();
+
+const sleep = (miliseconds: number) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(`Promise is resolved in ${miliseconds} miliseconds`);
+    }, miliseconds);
+  });
+};
+
+const callSleep = async (miliseconds: number) => {
+  console.log(await sleep(miliseconds));
+};
+
+callSleep(2000);
+
+const ping = (miliseconds: number, string: string) => {
+  console.log(`miliseconds ${miliseconds}, string ${string}`);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(string);
+    }, miliseconds);
+  });
+};
+
+//1 to 10 seconds range
+const getRandomMiliseconds = () => {
+  return Math.ceil(Math.random() * 10000);
+};
+
+const randomBatchPing = async (strings: string[]) => {
+  for (let string of strings) {
+    console.log(await ping(getRandomMiliseconds(), string));
+  }
+};
+
+randomBatchPing(["ping 1", "ping 2", "ping 3"]);
+
+const serializePings = async (strings: string[]) => {
+  const promiseArray = strings.map((string) =>
+    ping(getRandomMiliseconds(), string)
+  );
+
+  const results = await Promise.all(promiseArray);
+  console.log(results);
+};
+
+serializePings(["ping 1", "ping 2", "ping 3"]);
