7944f081913e37734318127e207d22483eea0b24
Julie Cherner <juliechernen@gmail.com>
2023-06-13 13:23:36 +0300

    Task is removed to personal folder

--- src/julie/006.ts
+++ src/julie/006.ts
@@ -24,3 +24,50 @@ async function exampleUsage() {
 }
 
 exampleUsage();
+
+const sleep = (miliseconds: number) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(`Promise is resolved in ${miliseconds} miliseconds`);
+    }, miliseconds);
+  });
+};
+
+const callSleep = async (miliseconds: number) => {
+  console.log(await sleep(miliseconds));
+};
+
+callSleep(2000);
+
+const ping = (miliseconds: number, string: string) => {
+  console.log(`miliseconds ${miliseconds}, string ${string}`);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(string);
+    }, miliseconds);
+  });
+};
+
+//1 to 10 seconds range
+const getRandomMiliseconds = () => {
+  return Math.ceil(Math.random() * 10000);
+};
+
+const randomBatchPing = async (strings: string[]) => {
+  for (let string of strings) {
+    console.log(await ping(getRandomMiliseconds(), string));
+  }
+};
+
+randomBatchPing(["ping 1", "ping 2", "ping 3"]);
+
+const serializePings = async (strings: string[]) => {
+  const promiseArray = strings.map((string) =>
+    ping(getRandomMiliseconds(), string)
+  );
+
+  const results = await Promise.all(promiseArray);
+  console.log(results);
+};
+
+serializePings(["ping 1", "ping 2", "ping 3"]);
